fd0317f59a5cfa2befa4e1da4dc7b6f572748eb5
Christine Dodrill <me@christine.website>
2020-08-12 07:48:41 -0400

    start arg structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+    /// File used for key/value storage
+    #[structopt(short, long, env = "KV_PATH", default_value = "./var/sled.db")]
+    kv_path: PathBuf,
+
+    /// SQLite database
+    #[structopt(short, long, env = "SQLITE_DB_PATH", default_value = "./var/data.db")]
+    sqlite_path: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
 }
