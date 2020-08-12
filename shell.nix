153cbbe1722586f4fb2785fb492ba44ba5845d51
Christine Dodrill <me@christine.website>
2020-08-12 10:07:54 -0400

    add wasm target

--- shell.nix
+++ shell.nix
@@ -2,13 +2,9 @@ let
   sources = import ./nix/sources.nix;
   moz_overlay = import sources.nixpkgs-mozilla;
   pkgs = import sources.nixpkgs { overlays = [ moz_overlay ]; };
+  rust = pkgs.rustChannelOfTargets "stable" null [ "wasm32-unknown-unknown" ];
 in pkgs.mkShell {
-  buildInputs = with pkgs; [
-    latest.rustChannels.stable.rust
-    bashInteractive
-    openssl
-    pkg-config
-  ];
+  buildInputs = with pkgs; [ rust bashInteractive openssl pkg-config ];
 
   RUST_LOG = "info";
 }
